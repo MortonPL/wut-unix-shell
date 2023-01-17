c3720bca220966e894c32cea4b72484c399df9d9
Dove6 <Dove6@users.noreply.github.com>
2023-01-17 01:51:16 +0100

    Uncomment tests

--- test/interpreter.c
+++ test/interpreter.c
@@ -1,33 +1,40 @@
-#include <tau/tau.h>
+#include "tau/tau/tau.h"
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
 #include <sys/file.h>
 #include <sys/stat.h>
-#include "../src/interpreter/interpreter.c"
+#include "../src/interpreter/interpreter.h"
 
-int CHILD_PID = -1;
+
+const int CHILD_PID = -1;
+const int COMMAND_SIZE = 256;
 
 void runInterpreter(char* command) {
-    MemContext context = MakeContext();
-    Env env;
-    env.cwd = (char*) AutoMalloc(context, COMMAND_SIZE, free);
-    env.childPid = &CHILD_PID;
-    env.variableCount = 0;
-    if (getcwd(env.cwd, COMMAND_SIZE) == NULL) {
-        exit(EXIT_FAILURE);
-    }
-    strcpy(env.previousWorkingDirectory, env.cwd);
+    // MemContext context = MakeContext();
+    // Env env;
+    // env.cwd = (char*) AutoMalloc(context, COMMAND_SIZE, free);
+    // env.childPid = &CHILD_PID;
+    // env.variableCount = 0;
+    // if (getcwd(env.cwd, COMMAND_SIZE) == NULL) {
+    //     exit(EXIT_FAILURE);
+    // }
+    // strcpy(env.previousWorkingDirectory, env.cwd);
+
+    ExecutionCtx ectx = {
+        .next_pipe_in = -1,
+    };
 
     LexerState lexerState;
     InitializeLexer(&lexerState, command);
     PipeExpression *expression = ReadPipeExpression(&lexerState);
     while (expression != NULL) {
-        interpret(expression, &env);
+        interpret(expression, &ectx);
         DeletePipeExpression(expression);
         expression = ReadPipeExpression(&lexerState);
     }
-    AutoExit(context);
+
+    // AutoExit(context);
 }
 
 TEST(InterpreterTestSuite, internalFunTest) {
@@ -56,18 +63,18 @@ TEST(InterpreterTestSuite, externalFunTest) {
     runInterpreter("rm -r aVeryTempDir");
 }
 
-// TEST(InterpreterTestSuite, redirectingTest) {
-//     runInterpreter("echo aaa > aVeryTempFile.txt");
-//     int fileDescriptor = open("aVeryTempFile.txt", O_RDONLY);
-//     char buffer[64] = {0}; 
-//     read(fileDescriptor, buffer, 64);
-//     CHECK_STREQ(buffer, "aaa");
-//     close(fileDescriptor);
-//     runInterpreter("rm aVeryTempFile");
-// }
+TEST(InterpreterTestSuite, redirectingTest) {
+    runInterpreter("echo aaa > aVeryTempFile.txt");
+    int fileDescriptor = open("aVeryTempFile.txt", O_RDONLY);
+    char buffer[64] = {0}; 
+    read(fileDescriptor, buffer, 64);
+    CHECK_STREQ(buffer, "aaa");
+    close(fileDescriptor);
+    runInterpreter("rm aVeryTempFile");
+}
 
-// TEST(InterpreterTestSuite, fileAccessTest) {
-//     fclose(fopen("aVeryTempFile.txt", "r"));
-//     CHECK_EQ(chmod("aVeryTempFile.txt", 000), 0);
-//     runInterpreter("cat aVeryTempFile.txt");
-// }
+TEST(InterpreterTestSuite, fileAccessTest) {
+    fclose(fopen("aVeryTempFile.txt", "r"));
+    CHECK_EQ(chmod("aVeryTempFile.txt", 000), 0);
+    runInterpreter("cat aVeryTempFile.txt");
+}
